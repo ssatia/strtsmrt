bfb4002948145d598beeb5e8ca0ce5f886f97c8f
7Aces <sanyam.satia@ymail.com>
2016-09-15 14:20:23 -0500

    Add a stock list and parser

---
+++ stocklistparser.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+  int i;
+  string s;
+  bool column_name = 1;
+
+  while(getline(cin, s)) {
+    for(i=0;i<s.length();++i) {
+      if(s[i]=='|') break;
+    }
+    if(!column_name) {
+      cout<<s.substr(0,i)<<endl;
+    }
+    else {
+      column_name = 0;
+    }
+  }
+
+  return 0;
+}
