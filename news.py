5f943a0507446c2eae4de85564007e12bd5ccced
7Aces <sanyam.satia@ymail.com>
2016-10-08 18:49:58 -0500

    Implemented news parser

--- news.py
+++ news.py
@@ -1,30 +1,44 @@
-import urllib2
-import xmltodict
+from bs4 import BeautifulSoup
+import requests
+import datetime
 
-def getNewsForSymbol(symbol):
-    file = urllib2.urlopen('http://feeds.finance.yahoo.com/rss/2.0/headline?s=' + symbol + '&region=US&lang=en-US')
-    data = file.read()
+FNAME = "test_stocks.txt"
+stocks = []
+
+def getNewsForDate(date):
+    file = open('data/news/' + date.strftime('%Y-%m-%d') + '.csv', 'w')
+    print 'Getting news for ' + date.strftime('%Y-%m-%d')
+    for i in range(len(stocks)):
+        query = 'http://www.reuters.com/finance/stocks/companyNews?symbol=' + stocks[i] + '&date=' + format(date.month, '02d') + format(date.day, '02d') + str(date.year)
+        print 'Getting news for ' + stocks[i]
+        print query
+        response = requests.get(query)
+        soup = BeautifulSoup(response.text, "html.parser")
+        divs = soup.findAll('div', {'class': 'feature'})
+        print 'Found ' + str(len(divs)) + ' articles.'
+        data = ''
+        for div in divs:
+            data = data.join(div.findAll(text=True))
+
+        file.write(stocks[i] + ',' + str(len(divs)) + ',' + data.replace('\n', ' '))
+        file.write('\n')
     file.close()
 
-    data = xmltodict.parse(data)
-    data = data['rss']['channel']['item']
-    headlines = ''
-
-    for i in range(len(data)):
-        headlines += data[i]['title']
-
-    return headlines
-
 def getNews():
-    news = []
-    filename = 'test_stocks.txt'
-    with open(filename) as f:
-        symbols = f.readlines()
+    date = datetime.date(2013, 1, 1)
+    endDate = datetime.date(2013, 1, 3)
 
-    for i in range(len(symbols)):
-        symbol = symbols[i].rstrip('\n')
-        news.append((symbol, getNewsForSymbol(symbol)))
+    while(date <= endDate):
+        getNewsForDate(date)
+        date += datetime.timedelta(days=1)
 
-    return news
+def init():
+    global stocks
+    with open(FNAME) as f:
+        stocks = f.readlines()
+    for i in range(len(stocks)):
+        stocks[i] = stocks[i].rstrip('\n')
 
-# getNews()
+    getNews()
+
+init()
