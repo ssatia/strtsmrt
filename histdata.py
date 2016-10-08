15cc76d2de7998a976a7e32bf9c67774ab3d17d7
7Aces <sanyam.satia@ymail.com>
2016-10-08 15:35:28 -0500

    Added historical stock data

---
+++ histdata.py
@@ -0,0 +1,23 @@
+import requests
+
+FNAME = "nasdaqlisted_formatted.txt"
+
+def getHistData():
+    DAY = '01'
+    MONTH = '00' # month must be m - 1
+    YEAR = '2010'
+
+    with open(FNAME) as f:
+        stocks = f.readlines()
+
+    for i in range(len(stocks)):
+        stock = stocks[i].rstrip('\n')
+        query = 'http://ichart.finance.yahoo.com/table.csv?s=' + stock + '&a=' + MONTH + '&b=' + DAY + '&c=' + YEAR
+        print 'Getting historical data for ' + stock
+        print query
+        response = requests.get(query)
+        file = open('data/hsd/' + stock + '.csv', 'w')
+        file.write(response.text)
+        file.close()
+
+getHistData()
