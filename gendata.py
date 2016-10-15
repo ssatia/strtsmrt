9f459f94f24e67c3548ac57db8ddb090428b4f67
7Aces <sanyam.satia@ymail.com>
2016-10-14 22:06:39 -0500

    Modified test data generator

--- gendata.py
+++ gendata.py
@@ -1,6 +1,9 @@
 import csv
 import datetime
 import sentiment
+import sys
+
+csv.field_size_limit(sys.maxint)
 
 def getStockData(symbol, date):
     file = open('data/hsd/' + symbol + '.csv')
@@ -11,21 +14,29 @@ def getStockData(symbol, date):
 
     data = []
 
+    print "Getting stock data for " + symbol + " for " + date.strftime('%Y-%m-%d')
+
     for row in csv_file:
         if(row[0] == date.strftime('%Y-%m-%d')):
             data.append(float(row[1]))
             data.append(float(row[4]))
             return data
 
+    # No data found for symbol for given date
+    return -1
+
 def genData():
     data_file = open('data/dat.csv', 'w+')
     csv_writer = csv.writer(data_file)
     date = datetime.date(2013, 1, 1)
-    endDate = datetime.date(2013, 1, 2)
+    endDate = datetime.date(2013, 6, 30)
 
     while(date <= endDate):
+        print "Checking data for " + date.strftime('%Y-%m-%d')
+
         day = date.weekday()
         if(day == 4 or day == 5):
+            date += datetime.timedelta(days=1)
             continue
 
         fname = date.strftime('%Y-%m-%d')
@@ -33,8 +44,11 @@ def genData():
         csv_file = csv.reader(file)
 
         for row in csv_file:
-            sentdata = sentiment.analyzeText(row[1])
             stockdata = getStockData(row[0], date)
+            if(stockdata == -1):
+                continue
+            sentdata = sentiment.analyzeText(row[1])
+
             data = []
             data.extend((row[0], date.timetuple().tm_yday))
             data.extend((sentdata['probability']['neg'], sentdata['probability']['neutral'], sentdata['probability']['pos']))
--- sentiment.py
+++ sentiment.py
@@ -2,6 +2,7 @@ import requests
 import json
 
 def analyzeText(text):
+    print "Performing sentiment analysis."
     jsonResponse = requests.post('http://text-processing.com/api/sentiment/', data = {'text': text})
     response = json.loads(jsonResponse.text)
     return response
