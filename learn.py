03c6293a4911390a158594fcfaa06f522a908572
7Aces <sanyam.satia@ymail.com>
2017-02-09 00:52:59 -0600

    Use one hot encoding for stock symbol and day parameters

--- learn.py
+++ learn.py
@@ -1,8 +1,7 @@
 import csv
 import numpy as np
-from sklearn.cross_validation import train_test_split
-from sklearn.preprocessing import normalize
-from sklearn.preprocessing import OneHotEncoder
+from sklearn import cross_validation
+from sklearn import preprocessing
 import tensorflow as tf
 
 def getData():
@@ -10,25 +9,42 @@ def getData():
         reader = csv.reader(csv_file)
         data = list(reader)
 
-    for i in range(len(data)):
-        data[i] = data[i][2:]
-
     return data
 
+def preprocessData(data):
+    label_encoder = preprocessing.LabelEncoder()
+    one_hot_encoder = preprocessing.OneHotEncoder()
+
+    data[:,0] = label_encoder.fit_transform(data[:,0])
+    data = data.astype(float)
+
+    # Uncomment lines below to use stock symbol and day parameters
+    # WARNING: Epochs may be extremely slow
+    # processed_data = one_hot_encoder.fit_transform(data[:,0:2]).toarray()
+    # processed_data = np.append(processed_data, data[:,2:6], 1)
+
+    # Do not use stock symbol and day parameters for training
+    processed_data = data[:,2:6]
+
+    processed_data = preprocessing.normalize(processed_data)
+    np.random.shuffle(processed_data)
+
+    return processed_data
+
 def learn(data):
-    data = normalize(data)
-    np.random.shuffle(data)
+    data = preprocessData(data)
+    num_params = data.shape[1] - 1
 
-    X = data[:,0:3]
-    Y = data[:,3].reshape(-1, 1)
+    X = data[:,0:num_params]
+    Y = data[:,num_params].reshape(-1, 1)
 
-    train_X, test_X, train_Y, test_Y = train_test_split(X, Y, test_size=0.30)
+    train_X, test_X, train_Y, test_Y = cross_validation.train_test_split(X, Y, test_size=0.30)
     training_data_size = train_X.shape[0]
 
-    stock_data = tf.placeholder(tf.float32, [None, 3])
+    stock_data = tf.placeholder(tf.float32, [None, num_params])
     stock_price = tf.placeholder(tf.float32, [None, 1])
 
-    W = tf.Variable(tf.zeros([3, 1], dtype=tf.float32))
+    W = tf.Variable(tf.zeros([num_params, 1], dtype=tf.float32))
     y = tf.matmul(stock_data, W)
 
     learning_rate = 1e-3
@@ -64,15 +80,15 @@ def learn(data):
 
     avg_perc_error = 0
     for i in range(len(test_Y)):
-        actual_change = abs(test_Y[i][0] - test_X[i][2]) / test_X[i][2]
-        predicted_change = abs(test_results[i][0] - test_X[i][2]) / test_X[i][2]
+        actual_change = abs(test_Y[i][0] - test_X[i][num_params - 1]) / test_X[i][num_params - 1]
+        predicted_change = abs(test_results[i][0] - test_X[i][num_params - 1]) / test_X[i][num_params - 1]
         avg_perc_error = avg_perc_error + abs(actual_change - predicted_change)
 
     avg_perc_error = (avg_perc_error * 100) / len(test_Y)
     print "Average percentage error: ", avg_perc_error
 
 def main():
-    data = np.array(getData()).astype(float)
+    data = np.array(getData())
     learn(data)
 
 main()
