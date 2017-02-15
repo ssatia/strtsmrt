734917da14ce4355eb6dff819d6270a7d70707de
7Aces <sanyam.satia@ymail.com>
2017-02-15 04:04:57 -0600

    Add neural network

--- README.md
+++ README.md
@@ -5,8 +5,8 @@ strtsmrt is a machine learning system that predicts stock prices based on news a
 ##How it works:
 
 * Gets sentiment analysis data on news associated to stocks in the S&P 500 (using Google's Natural Language Cloud API)
-* Performs gradient descent on a multiple linear regression learning model using TensorFlow
+* Performs gradient descent on a neural network learning model using TensorFlow
 * Uses historical stock data fetched from Yahoo! Finance for training and testing purposes
 
 ##Results:
-Using a day's news sources, strtsmrt predicts the next day's stock price with an average percentage error of __0.8973%__ using the linear regression model.
+Using a day's news sources, strtsmrt predicts the next day's stock price with an average percentage error of __0.5349%__ using the neural network model with two hidden layers.
---
+++ nn.py
@@ -0,0 +1,113 @@
+import csv
+import numpy as np
+from sklearn import cross_validation
+from sklearn import preprocessing
+import tensorflow as tf
+
+def getData():
+    with open('data/dat_0.csv') as csv_file:
+        reader = csv.reader(csv_file)
+        data = list(reader)
+
+    return data
+
+def preprocessData(data):
+    label_encoder = preprocessing.LabelEncoder()
+    one_hot_encoder = preprocessing.OneHotEncoder()
+
+    data[:,0] = label_encoder.fit_transform(data[:,0])
+    data = data.astype(float)
+
+    # Uncomment lines below to use stock symbol and day parameters
+    # WARNING: Epochs may be extremely slow
+    # processed_data = one_hot_encoder.fit_transform(data[:,0:2]).toarray()
+    # processed_data = np.append(processed_data, data[:,2:6], 1)
+
+    # Do not use stock symbol and day parameters for training
+    processed_data = data[:,2:6]
+
+    processed_data = preprocessing.normalize(processed_data)
+    np.random.shuffle(processed_data)
+
+    return processed_data
+
+def learn(data):
+    data = preprocessData(data)
+    num_params = data.shape[1] - 1
+
+    X = data[:,0:num_params]
+    Y = data[:,num_params].reshape(-1, 1)
+
+    train_X, test_X, train_Y, test_Y = cross_validation.train_test_split(X, Y, test_size=0.30)
+    training_data_size = train_X.shape[0]
+
+    stock_data = tf.placeholder(tf.float32, [None, num_params])
+    stock_price = tf.placeholder(tf.float32, [None, 1])
+
+    # Number of neurons in the hidden layer
+    n_hidden_1 = 3
+    n_hidden_2 = 3
+
+    weights = {
+        'h1': tf.Variable(tf.random_normal([num_params, n_hidden_1])),
+        'h2': tf.Variable(tf.random_normal([n_hidden_1, n_hidden_2])),
+        'out': tf.Variable(tf.random_normal([n_hidden_2, 1]))
+    }
+    biases = {
+        'b1': tf.Variable(tf.random_normal([n_hidden_1])),
+        'b2': tf.Variable(tf.random_normal([n_hidden_2])),
+        'out': tf.Variable(tf.random_normal([1]))
+    }
+
+    layer_1 = tf.add(tf.matmul(stock_data, weights['h1']), biases['b1'])
+    layer_1 = tf.nn.relu(layer_1)
+    layer_2 = tf.add(tf.matmul(layer_1, weights['h2']), biases['b2'])
+    layer_2 = tf.nn.relu(layer_2)
+
+    output_layer = tf.matmul(layer_2, weights['out'])
+
+    learning_rate = 1e-3
+    cost_function = tf.reduce_mean(tf.pow(stock_price - output_layer, 2))
+    optimizer = tf.train.AdamOptimizer(learning_rate).minimize(cost_function)
+
+    last_cost = 0
+    tolerance = 1e-6
+    epochs = 1
+    max_epochs = 1e6
+
+    sess = tf.Session()
+    with sess.as_default():
+        init = tf.initialize_all_variables()
+        sess.run(init)
+
+        while True:
+            sess.run(optimizer, feed_dict={stock_data: train_X, stock_price: train_Y})
+
+            if epochs % 100 == 0:
+                cost = sess.run(cost_function, feed_dict={stock_data: train_X, stock_price: train_Y})
+                print "Epoch: %d: Error: %.4f" %(epochs, cost)
+
+                if abs(cost - last_cost) <= tolerance or epochs > max_epochs:
+                    print "Converged."
+                    break
+                last_cost = cost
+
+            epochs += 1
+
+        print "Test cost: ", sess.run(cost_function, feed_dict={stock_data: test_X, stock_price: test_Y})
+        test_results = sess.run(output_layer, feed_dict={stock_data: test_X, stock_price: test_Y})
+
+    avg_perc_error = 0
+    for i in range(len(test_Y)):
+        actual_change = abs(test_Y[i][0] - test_X[i][num_params - 1]) / test_X[i][num_params - 1]
+        predicted_change = abs(test_results[i][0] - test_X[i][num_params - 1]) / test_X[i][num_params - 1]
+        avg_perc_error = avg_perc_error + abs(actual_change - predicted_change)
+
+    avg_perc_error = (avg_perc_error * 100) / len(test_Y)
+    print "Average percentage error: ", avg_perc_error
+
+def main():
+    data = np.array(getData())
+    learn(data)
+
+main()
